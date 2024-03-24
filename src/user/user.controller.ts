3631553bcad4b386f97d50f8cb67331f722a76f0
thx4u <48453875+thxForu@users.noreply.github.com>
2024-03-25 01:50:56 +0200

    add auth and pagination

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,51 @@
-import { Body, Controller, Post, Get, Param } from '@nestjs/common';
-import { User, Prisma } from '@prisma/client';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { Investor, Partner, Prisma, Startup, User } from '@prisma/client';
 import { UserService } from './user.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {
+  }
 
   @Post()
-  async createUser(@Body() userData: Prisma.UserCreateInput): Promise<User> {
-    return this.userService.createUser(userData);
+  @HttpCode(HttpStatus.CREATED)
+  async createUser(@Body() userData: Prisma.UserCreateInput): Promise<void> {
+    await this.userService.createUser(userData);
+  }
+
+  @Get('investors')
+  @UseGuards(AuthGuard)
+  async getInvestors(@Query('limit') limit: number, @Query('offset') offset: number): Promise<Investor[]> {
+    return this.userService.getInvestors(limit, offset);
+  }
+
+  @Get('startups')
+  @UseGuards(AuthGuard)
+  async getStartups(@Query('limit') limit: number, @Query('offset') offset: number): Promise<Startup[]> {
+    return this.userService.getStartups(limit, offset);
+  }
+
+  @Get('partners')
+  @UseGuards(AuthGuard)
+  async getPartners(@Query('limit') limit: number, @Query('offset') offset: number): Promise<Partner[]> {
+    return this.userService.getPartners(limit, offset);
   }
 
   @Get(':id')
-  async findUserById(@Param('id') id: number): Promise<User> {
+  @UseGuards(AuthGuard)
+  async findUserById(@Param('id', ParseIntPipe) id: number): Promise<User> {
     return this.userService.findUserById(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,23 +1,34 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
-import { PrismaService } from "../databases/prisma.service";
-import { Prisma, User } from "@prisma/client";
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../databases/prisma.service';
+import { Investor, Partner, Prisma, Startup, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {
   }
 
-  async createUser(userInputData: Prisma.UserCreateInput): Promise<User> {
+  async createUser(userInputData: Prisma.UserCreateInput): Promise<void> {
+    const { email, password } = userInputData;
+    const existingUser = await this.findOne(email);
+
+    if (existingUser) {
+      throw new BadRequestException('User with this email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    userInputData = { ...userInputData, password: hashedPassword };
+
     const { role } = userInputData;
 
-    if (role === "INVESTOR") {
-      return this.createInvestor(userInputData);
-    } else if (role === "STARTUP") {
-      return this.createStartup(userInputData);
-    } else if (role === "PARTNER") {
-      return this.createPartner(userInputData);
+    if (role === 'INVESTOR') {
+      await this.createInvestor(userInputData);
+    } else if (role === 'STARTUP') {
+      await this.createStartup(userInputData);
+    } else if (role === 'PARTNER') {
+      await this.createPartner(userInputData);
     } else {
-      throw new BadRequestException("Invalid user role");
+      throw new BadRequestException('Invalid user role');
     }
   }
 
@@ -26,11 +37,11 @@ export class UserService {
     const user = await this.prisma.user.create({
       data: {
         ...userData,
-        role: "INVESTOR",
+        role: 'INVESTOR',
         investor: {
-          create: userData.investor as Prisma.InvestorCreateInput
-        }
-      }
+          create: userData.investor as Prisma.InvestorCreateInput,
+        },
+      },
     });
 
     return user;
@@ -40,11 +51,11 @@ export class UserService {
     const user = await this.prisma.user.create({
       data: {
         ...userData,
-        role: "STARTUP",
+        role: 'STARTUP',
         startup: {
-          create: userData.startup as Prisma.StartupCreateInput
-        }
-      }
+          create: userData.startup as Prisma.StartupCreateInput,
+        },
+      },
     });
 
     return user;
@@ -54,21 +65,51 @@ export class UserService {
     const user = await this.prisma.user.create({
       data: {
         ...userData,
-        role: "PARTNER",
+        role: 'PARTNER',
         partner: {
-          create: userData.partner as Prisma.PartnerCreateInput
-        }
-      }
+          create: userData.partner as Prisma.PartnerCreateInput,
+        },
+      },
     });
 
     return user;
   }
 
+
+  async getInvestors(limit: number, offset: number): Promise<Investor[]> {
+    return this.prisma.investor.findMany({
+      take: limit,
+      skip: offset,
+    });
+  }
+
+  async getStartups(limit: number, offset: number): Promise<Startup[]> {
+    return this.prisma.startup.findMany({
+      take: limit,
+      skip: offset,
+    });
+  }
+
+  async getPartners(limit: number, offset: number): Promise<Partner[]> {
+    return this.prisma.partner.findMany({
+      take: limit,
+      skip: offset,
+    });
+  }
+
   async findUserById(id: number): Promise<User | null> {
     return this.prisma.user.findUnique({
       where: {
-        id
-      }
+        id,
+      },
+    });
+  }
+
+  async findOne(email: string): Promise<User | undefined> {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
     });
   }
 }
